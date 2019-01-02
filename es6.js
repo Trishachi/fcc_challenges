fe173990ae31a9175cb8824c77f7f9bec4a9fc6a
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-01-02 07:53:32 -0700

    Rest operator

--- es6.js
+++ es6.js
@@ -22,19 +22,32 @@
 // The reduce() method reduces the array to a single value. It executes a provided function for each value of the array (from left-to-right).
 // Syntax - array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
 
+//Default parameters - sets in when a value is not specified for an arguement
+//Rest Operator - With the rest operator, you can create functions that take a variable number of arguments.
+
+
 
 // Codes
 
 // ES6- 1: Write Higher Order Arrow Functions
-const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
-const squareList = (arr) => {
+// const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34, -2];
+// const squareList = (arr) => {
+//   "use strict";
+//   // change code below this line
+//   const squaredIntegers = arr.filter( (num) => (Number.isInteger(num) && (num > 0)))
+//                              .map((num) => Math.pow(num, 2) );
+//   // change code above this line
+//   return squaredIntegers;
+// };
+// // test your code
+// const squaredIntegers = squareList(realNumberArray);
+// console.log(squaredIntegers);
+
+// ES6 - 2: Use the Rest Operator with Function Parameters
+const sum = (function() {
   "use strict";
-  // change code below this line
-  const squaredIntegers = arr.filter( (num) => (Number.isInteger(num) && (num > 0)))
-                             .map((num) => Math.pow(num, 2) );
-  // change code above this line
-  return squaredIntegers;
-};
-// test your code
-const squaredIntegers = squareList(realNumberArray);
-console.log(squaredIntegers);
+  return function sum(...args) {
+    return args.reduce((a, b) => a + b, 0);
+  };
+})();
+console.log(sum(1, 2, 3)); // 6
