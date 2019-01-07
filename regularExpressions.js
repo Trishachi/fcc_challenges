db3eefd93abbadbcd89a46d3040a14c77820954c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-01-06 23:31:59 -0700

    Regular Expressions - Day 2

--- regularExpressions.js
+++ regularExpressions.js
@@ -8,11 +8,19 @@
 // The .test() method takes the regex, applies it to a string (which is placed inside the parentheses),
 // and returns true or false if your pattern finds something or not.
 
+//In regular expressions, a greedy match finds the longest possible part of a string that fits the regex pattern
+//and returns it as a match. The alternative is called a lazy match, which finds the smallest possible part of the
+//string that satisfies the regex pattern.
+
 //Regular expressions modifiers and patterns
 //i	- Perform case-insensitive matching
 //g - Perform a global match (find all matches rather than stopping after the first match)
 //m	- Perform multiline matching
 //. - The wildcard character . will match any one character. eg /hu./
+//n+ -	Matches any string that contains at least one n
+//n* -	Matches any string that contains zero or more occurrences of n
+//n? - Lazy matching - Matches any string that contains zero or one occurrences of n
+
 
 
 //Codes
@@ -70,7 +78,31 @@
 // Regular Expressions - 8: Match Numbers and Letters of the Alphabet
 // Create a single regex that matches a range of letters between h and s, and
 // a range of numbers between 2 and 6. Remember to include the appropriate flags in the regex.
-let quoteSample = "Blueberry 3.141592653s are delicious.";
-let myRegex = /[h-s2-6]/gi; // Change this line
-let result = quoteSample.match(myRegex); // Change this line
+// let quoteSample = "Blueberry 3.141592653s are delicious.";
+// let myRegex = /[h-s2-6]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+// Regular Expressions - 9: Match Single Characters Not Specified == negated character sets
+// Create a single regex that matches all characters that are not a number or a vowel.
+// Remember to include the appropriate flags in the regex.
+// let quoteSample = "3 blind mice.";
+// let myRegex = /[^aeiou0-9]/gi; // Change this line
+// let result = quoteSample.match(myRegex); // Change this line
+// console.log(result);
+
+// Regular Expressions - 10: Match Characters that Occur Zero or More Times
+// Create a regex chewieRegex that uses the * character to match all the upper and lower"a" characters in chewieQuote. Your
+// regex does not need flags, and it should not match any of the other quotes.
+// let chewieQuote = "Aaaaaaaaaaaaaaaarrrgh!";
+// let chewieRegex = /Aa*/; // Change this line
+// let result = chewieQuote.match(chewieRegex);
+// console.log(result);
+
+// Regular Expressions - 11: Find Characters with Lazy Matching
+// Fix the regex /<.*>/ to return the HTML tag <h1> and not the text "<h1>Winter is coming</h1>".
+// Remember the wildcard . in a regular expression matches any character.
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let result = text.match(myRegex);
 console.log(result);
