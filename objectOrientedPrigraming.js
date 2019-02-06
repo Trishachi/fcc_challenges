165cd732c7e0cee3c77061ba8a785c608ae3566d
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-02-05 18:11:12 -0700

    OOP - Basic JS Object

---
+++ objectOrientedPrigraming.js
@@ -0,0 +1,18 @@
+//Notes
+//object oriented programming organizes code into object definitions sometimes called classes
+//The data is an object's attributes, and the behavior (or functions) are methods
+
+//Properties - Classes, Instances, Inheritance
+
+
+
+
+//Codes
+//Object Oriented Programming: Create a Basic JavaScript Object
+//Create a dog object with name and numLegs properties, and set them to a string and a number, respectively.
+let dog = {
+  name: "Puma",
+  numLegs: 4
+};
+
+console.log(dog.name);
