05b5b33b0ada85931bcde550a96e7b5a70adb110
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-07-31 22:00:26 -0600

    Functional Programming - Day 4

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -450,3 +450,52 @@
 //   return item % 2 === 1;
 // });
 // console.log(new_s);
+
+// Functional Programming: Return Part of an Array Using the slice Method
+// Use the slice method in the sliceArray function to return part of the anim
+// array given the provided beginSlice and endSlice indices. The function should return an array
+// function sliceArray(anim, beginSlice, endSlice) {
+//   // Add your code below this line
+//   return anim.slice(beginSlice, endSlice)
+//   // Add your code above this line
+// }
+// var inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+// console.log(sliceArray(inputAnim, 1, 4));
+
+// Functional Programming: Remove Elements from an Array Using slice Instead of splice
+// Rewrite the function nonMutatingSplice by using slice instead of splice. It should limit
+// the provided cities array to a length of 3, and return a new array with only the first three items.
+// Do not mutate the original array provided to the function.
+//
+// function nonMutatingSplice(cities) {
+//   // Add your code below this line
+//   return cities.slice(0, 3);
+//   // Add your code above this line
+// }
+// var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+// console.log(nonMutatingSplice(inputCities));
+
+// Functional Programming: Combine Two Arrays Using the concat Method
+// Use the concat method in the nonMutatingConcat function to concatenate
+// attach to the end of original. The function should return the concatenated array.
+// function nonMutatingConcat(original, attach) {
+//   // Add your code below this line
+//     // return [...first, ...second]; //This also works
+//     return first.concat(second);
+//   // Add your code above this line
+// }
+// var first = [1, 2, 3];
+// var second = [4, 5];
+// console.log(nonMutatingConcat(first, second));
+
+// Functional Programming: Add Elements to the End of an Array Using concat Instead of push
+// Change the nonMutatingPush function so it uses concat to add newItem to the end of original
+// instead of push. The function should return an array.
+function nonMutatingPush(original, newItem) {
+  // Add your code below this line
+  return original.concat(newItem);
+  // Add your code above this line
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+console.log(nonMutatingPush(first, second));
