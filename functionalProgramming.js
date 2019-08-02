f4d74fefd747620449a8e8b6d351eb1a72cdd9f9
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-08-02 00:06:23 -0600

    Functional Programming - Day 5

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -9,7 +9,9 @@
 //Hence - Try not to alter any variable or object in our code.
 
 // map is a pure function, and its output depends solely on its inputs. Plus, it takes another function as its argument.
-
+//A side effect of the sort method is that it changes the order of the elements in the original array. In other words,
+// it mutates the array in place. One way to avoid this is to first concatenate an empty array to the one being sorted
+// (remember that concat returns a new array), then run the sort method.
 
 //Codes
 
@@ -491,11 +493,172 @@
 // Functional Programming: Add Elements to the End of an Array Using concat Instead of push
 // Change the nonMutatingPush function so it uses concat to add newItem to the end of original
 // instead of push. The function should return an array.
-function nonMutatingPush(original, newItem) {
-  // Add your code below this line
-  return original.concat(newItem);
-  // Add your code above this line
-}
-var first = [1, 2, 3];
-var second = [4, 5];
-console.log(nonMutatingPush(first, second));
+// function nonMutatingPush(original, newItem) {
+//   // Add your code below this line
+//   return original.concat(newItem);
+//   // Add your code above this line
+// }
+// var first = [1, 2, 3];
+// var second = [4, 5];
+// console.log(nonMutatingPush(first, second));
+
+// Functional Programming: Use the reduce Method to Analyze Data
+// The variable watchList holds an array of objects with information on several movies.
+// Use reduce to find the average IMDB rating of the movies directed by Christopher Nolan.
+
+// the global variable
+// var watchList = [
+//                  {
+//                    "Title": "Inception",
+//                    "Year": "2010",
+//                    "Rated": "PG-13",
+//                    "Released": "16 Jul 2010",
+//                    "Runtime": "148 min",
+//                    "Genre": "Action, Adventure, Crime",
+//                    "Director": "Christopher Nolan",
+//                    "Writer": "Christopher Nolan",
+//                    "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page, Tom Hardy",
+//                    "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+//                    "Language": "English, Japanese, French",
+//                    "Country": "USA, UK",
+//                    "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+//                    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+//                    "Metascore": "74",
+//                    "imdbRating": "8.8",
+//                    "imdbVotes": "1,446,708",
+//                    "imdbID": "tt1375666",
+//                    "Type": "movie",
+//                    "Response": "True"
+//                 },
+//                 {
+//                    "Title": "Interstellar",
+//                    "Year": "2014",
+//                    "Rated": "PG-13",
+//                    "Released": "07 Nov 2014",
+//                    "Runtime": "169 min",
+//                    "Genre": "Adventure, Drama, Sci-Fi",
+//                    "Director": "Christopher Nolan",
+//                    "Writer": "Jonathan Nolan, Christopher Nolan",
+//                    "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+//                    "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+//                    "Language": "English",
+//                    "Country": "USA, UK",
+//                    "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+//                    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+//                    "Metascore": "74",
+//                    "imdbRating": "8.6",
+//                    "imdbVotes": "910,366",
+//                    "imdbID": "tt0816692",
+//                    "Type": "movie",
+//                    "Response": "True"
+//                 },
+//                 {
+//                    "Title": "The Dark Knight",
+//                    "Year": "2008",
+//                    "Rated": "PG-13",
+//                    "Released": "18 Jul 2008",
+//                    "Runtime": "152 min",
+//                    "Genre": "Action, Adventure, Crime",
+//                    "Director": "Christopher Nolan",
+//                    "Writer": "Jonathan Nolan (screenplay), Christopher Nolan (screenplay), Christopher Nolan (story), David S. Goyer (story), Bob Kane (characters)",
+//                    "Actors": "Christian Bale, Heath Ledger, Aaron Eckhart, Michael Caine",
+//                    "Plot": "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, the caped crusader must come to terms with one of the greatest psychological tests of his ability to fight injustice.",
+//                    "Language": "English, Mandarin",
+//                    "Country": "USA, UK",
+//                    "Awards": "Won 2 Oscars. Another 146 wins & 142 nominations.",
+//                    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_SX300.jpg",
+//                    "Metascore": "82",
+//                    "imdbRating": "9.0",
+//                    "imdbVotes": "1,652,832",
+//                    "imdbID": "tt0468569",
+//                    "Type": "movie",
+//                    "Response": "True"
+//                 },
+//                 {
+//                    "Title": "Batman Begins",
+//                    "Year": "2005",
+//                    "Rated": "PG-13",
+//                    "Released": "15 Jun 2005",
+//                    "Runtime": "140 min",
+//                    "Genre": "Action, Adventure",
+//                    "Director": "Christopher Nolan",
+//                    "Writer": "Bob Kane (characters), David S. Goyer (story), Christopher Nolan (screenplay), David S. Goyer (screenplay)",
+//                    "Actors": "Christian Bale, Michael Caine, Liam Neeson, Katie Holmes",
+//                    "Plot": "After training with his mentor, Batman begins his fight to free crime-ridden Gotham City from the corruption that Scarecrow and the League of Shadows have cast upon it.",
+//                    "Language": "English, Urdu, Mandarin",
+//                    "Country": "USA, UK",
+//                    "Awards": "Nominated for 1 Oscar. Another 15 wins & 66 nominations.",
+//                    "Poster": "http://ia.media-imdb.com/images/M/MV5BNTM3OTc0MzM2OV5BMl5BanBnXkFtZTYwNzUwMTI3._V1_SX300.jpg",
+//                    "Metascore": "70",
+//                    "imdbRating": "8.3",
+//                    "imdbVotes": "972,584",
+//                    "imdbID": "tt0372784",
+//                    "Type": "movie",
+//                    "Response": "True"
+//                 },
+//                 {
+//                    "Title": "Avatar",
+//                    "Year": "2009",
+//                    "Rated": "PG-13",
+//                    "Released": "18 Dec 2009",
+//                    "Runtime": "162 min",
+//                    "Genre": "Action, Adventure, Fantasy",
+//                    "Director": "James Cameron",
+//                    "Writer": "James Cameron",
+//                    "Actors": "Sam Worthington, Zoe Saldana, Sigourney Weaver, Stephen Lang",
+//                    "Plot": "A paraplegic marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+//                    "Language": "English, Spanish",
+//                    "Country": "USA, UK",
+//                    "Awards": "Won 3 Oscars. Another 80 wins & 121 nominations.",
+//                    "Poster": "http://ia.media-imdb.com/images/M/MV5BMTYwOTEwNjAzMl5BMl5BanBnXkFtZTcwODc5MTUwMw@@._V1_SX300.jpg",
+//                    "Metascore": "83",
+//                    "imdbRating": "7.9",
+//                    "imdbVotes": "876,575",
+//                    "imdbID": "tt0499549",
+//                    "Type": "movie",
+//                    "Response": "True"
+//                 }
+// ];
+// // Add your code below this line
+// var averageRating = watchList.filter(item => item.Director === "Christopher Nolan").map(num => Number(num.imdbRating)).reduce((acc, cur) => acc + cur)/ watchList.filter(item => item.Director === "Christopher Nolan").length;
+// // Add your code above this line
+// console.log(averageRating);
+
+
+// Functional Programming: Sort an Array Alphabetically using the sort Method
+// Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+// function alphabeticalOrder(arr) {
+//   // Add your code below this line
+//   return arr.sort(function(a,b){
+//     // return a > b;
+//     return a > b ? 1 : -1
+//   });
+//   // Add your code above this line
+// }
+// console.log(alphabeticalOrder(["a", "a", "a", "a", "x", "t"]));
+
+
+// Functional Programming: Return a Sorted Array Without Changing the Original Array
+//Use the sort method in the nonMutatingSort function to sort the elements of an array
+//in ascending order. The function should return a new array, and not mutate the globalArray variable.
+// var globalArray = [5, 6, 3, 2, 9];
+// function nonMutatingSort(arr) {
+//   // Add your code below this line
+//   var newArray = [];
+//   return arr.concat(newArray).sort(function(a,b){
+//     return a - b;
+//   });
+//   // Add your code above this line
+// }
+// console.log(nonMutatingSort(globalArray));
+// console.log(globalArray);
+
+// Functional Programming: Split a String into an Array Using the split Method
+//Use the split method inside the splitify function to split str into an array
+//of words. The function should return the array. Note that the words are not always separated by spaces, and the array should not contain punctuation.
+// function splitify(str) {
+//   // Add your code below this line
+//   return str.split(/\W/);
+//   // Add your code above this line
+// }
+// console.log(splitify("Hello World,I-am code"));
