f417ebc09402832075c75808715327db80785e4a
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2019-08-13 21:07:35 -0600

    React - Day 2

--- React.js
+++ React.js
@@ -510,3 +510,63 @@
 //     );
 //   }
 // };
+
+// React - 23: Render State in the User Interface Another Way
+//In the MyComponent render method, define a const called name
+// and set it equal to the name value in the component's state.
+// Because you can write JavaScript directly in this part of the code,
+// you don't have to enclose this reference in curly braces.
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'freeCodeCamp'
+//     }
+//   }
+//   render() {
+//     // change code below this line
+//  const name = this.state.name;
+//     // change code above this line
+//     return (
+//       <div>
+//         { /* change code below this line */ }
+//         <h1>{ name }</h1>
+//         { /* change code above this line */ }
+//       </div>
+//     );
+//   }
+// };
+
+// React - 24: Set State with this.setState
+// There is a button element in the code editor
+// which has an onClick() handler. This handler is triggered
+// when the button receives a click event in the browser, and
+// runs the handleClick method defined on MyComponent. Within the
+// handleClick method, update the component state using this.setState().
+// Set the name property in state to equal the string React Rocks!.
+// class MyComponent extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       name: 'Initial State'
+//     };
+//     this.handleClick = this.handleClick.bind(this);
+//   }
+//   handleClick() {
+//     // change code below this line
+//     this.setState({
+//       name: 'React Rocks!'
+//     })
+//     // change code above this line
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.handleClick}>Click Me</button>
+//         <h1>{this.state.name}</h1>
+//       </div>
+//     );
+//   }
+// };
+
+React - 25: Bind 'this' to a Class Method
