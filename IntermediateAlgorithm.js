90e626117f4407ca2b6ee7a17ffc8e439df2552a
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-03-13 00:21:02 -0600

    Intermediate JS - Seek and Destroy

--- IntermediateAlgorithm.js
+++ IntermediateAlgorithm.js
@@ -1,5 +1,4 @@
 //Sum All Numbers in a Range
-
 // function sumAll(arr) {
 //   let total = 0;
 //   let start = Math.min(...arr);
@@ -12,22 +11,37 @@
 // console.log(sumAll([10, 5]));
 
 //Diff Two Arrays
-function diffArray(arr1, arr2) {
-  var newArr = [];
-  // arr1 = arr1.toString().split(',').map(Number);
-  // arr2 = arr2.toString().split(',').map(Number);
+// function diffArray(arr1, arr2) {
+//   var newArr = [];
+//   // arr1 = arr1.toString().split(',').map(Number);
+//   // arr2 = arr2.toString().split(',').map(Number);
+//
+//   for(var i in arr1){
+//     if(arr2.indexOf(arr1[i]) === -1){
+//       newArr.push(arr1[i]);
+//     }
+//   }
+//   for(i in arr2){
+//     if(arr1.indexOf(arr2[i]) === -1){
+//       newArr.push(arr2[i]);
+//     }
+//   }
+//   return newArr;
+// }
+// console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
 
-  for(var i in arr1){
-    if(arr2.indexOf(arr1[i]) === -1){
-      newArr.push(arr1[i]);
+
+//Intermediate Algorithm Scripting: Seek and Destroy
+function destroyer(arr) {
+  var args = Array.from(arguments)
+  var destroyed = []
+  args.splice(0,1)
+  for(var item of arr){
+    if(args.indexOf(item) === -1){
+      destroyed.push(item)
     }
   }
-  for(i in arr2){
-    if(arr1.indexOf(arr2[i]) === -1){
-      newArr.push(arr2[i]);
-    }
-  }
-  return newArr;
+  return destroyed;
 }
 
-console.log(diffArray(["andesite", "grass", "dirt", "pink wool", "dead shrub"], ["diorite", "andesite", "grass", "dirt", "dead shrub"]));
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
