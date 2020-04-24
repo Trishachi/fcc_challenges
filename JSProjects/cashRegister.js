862d1bb5dc37e39aec823cdc1fdd5e39a0d10fbc
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-04-23 22:15:01 -0600

    JS Project - Cash Register

--- JSProjects/cashRegister.js
+++ JSProjects/cashRegister.js
@@ -1,47 +1,60 @@
+const currency = [
+  {name: "ONE HUNDRED", value: 100.00},
+  {name: "TWENTY", value: 20.00},
+  {name: "TEN", value: 10.00},
+  {name: "FIVE", value: 5.00},
+  {name: "ONE", value: 1.00},
+  {name: "QUARTER", value: 0.25},
+  {name: "DIME", value: 0.10},
+  {name: "NICKEL", value: 0.05},
+  {name: "PENNY", value: 0.01}
+]
+
 function checkCashRegister(price, cash, cid) {
-  const currency = {
-    "PENNY": 0.01,
-    "NICKEL": 0.05,
-    "DIME": 0.1,
-    "QUARTER": 0.25,
-    "DOLLAR": 1.00,
-    "FIVE": 5.00,
-    "TEN": 10.00,
-    "TWENTY": 20.00,
-    "ONE HUNDRED": 100.00
+  const result = {
+    status: null,
+    change: []
   }
+  // let changeDue = parseFloat(cash - price).toFixed(2);
+  let changeDue = cash - price;
+  const availableChange = cid.reduce(function(accum, curr){
+    accum.total += curr[1];
+    accum[curr[0]] = curr[1];
+    return accum;
+  }, {total: 0});
 
-  let changeDue;
-  let cashAtHand = 0;
-  let result = {
-    status: "",
-    change: 0
-  }
-
-  function getCashAtHand(cid){
-    for(let i = 0; i < cid.length; i++){
-      // console.log(cid[i][1]);
-      cashAtHand += (cid[i][1]);
-    }
-    return cashAtHand.toFixed(2);
-  }
-
-  // console.log(getCashAtHand([["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]));
-
-  changeDue = cash - price;
-  if(getCashAtHand(cid) < changeDue){
+  if(availableChange.total < changeDue){
     result.status = "INSUFFICIENT_FUNDS";
     result.change = [];
+    return result;
   }
-  if(getCashAtHand(cid) == changeDue){
+  if(availableChange.total === changeDue){
     result.status = "CLOSED";
     result.change = [...cid];
+    return result;
   }
-  if(getCashAtHand(cid) > changeDue){
-    result.status = "OPEN";
-    result.change = changeDue;
+  let changeArray = currency.reduce(function(acc, cur){
+    let moneyValue = 0;
+    while(availableChange[cur.name] > 0 && changeDue >= cur.value){
+      changeDue -= cur.value;
+      availableChange[cur.name] -= cur.value;
+      moneyValue += cur.value;
+
+      changeDue = Math.round(changeDue * 100) / 100;
+    }
+    if(moneyValue > 0){
+      acc.push([cur.name, moneyValue]);
+    }
+    return acc;
+  }, []);
+  if(changeArray.length < 1 || changeDue > 0){
+    result.status = "INSUFFICIENT_FUNDS";
+    return result;
   }
+
+  result.status = "OPEN";
+  result.change = changeArray;
   return result;
 }
 
-console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
