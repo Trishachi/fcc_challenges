55613bbfcdd4fe86508693b76fc04635d34ca218
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-09 02:33:19 -0600

    Redux -  Define an Action Creator

--- Redux.js
+++ Redux.js
@@ -2,7 +2,8 @@
 //Redux is a state management framework that can be used with a number of different web technologies, including React.
 // The first important principle to understand when learning Redux: the Redux store is the single source of truth
 // when it comes to application state.
-You can retrieve the current state held in the Redux store object with the getState() method
+// You can retrieve the current state held in the Redux store object with the getState() method.
+// An action creator is simply a JavaScript function that returns an action.
 
 
 
@@ -21,9 +22,26 @@ You can retrieve the current state held in the Redux store object with the getSt
 // Redux - 2: Get State from the Redux Store
 // Use store.getState() to retrieve the state from the store,
 // and assign this to a new variable currentState.
-const store = Redux.createStore(
-  (state = 5) => state
-);
-// change code below this line
-let currentState = store.getState();
-console.log(currentState);
+// const store = Redux.createStore(
+//   (state = 5) => state
+// );
+// // change code below this line
+// let currentState = store.getState();
+// console.log(currentState);
+
+// Redux - 3: Define a Redux Action
+// Declare an object action and give it a property type set to the string 'LOGIN'.
+// Define an action here:
+// const action = {
+//   type: 'LOGIN'
+// }
+
+// Redux - 4: Define an Action Creator
+// Define a function named actionCreator() that returns the action object when called.
+// const action = {
+//   type: 'LOGIN'
+// }
+// // Define an action creator here:
+// function actionCreator(){
+//   return action;
+// }
