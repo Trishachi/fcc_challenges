34f9fc986086e0bf18ce8e36b4ff81582bbcbc2a
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-11 22:17:53 -0600

    Redux - Combine Multiple Reducers

--- Redux.js
+++ Redux.js
@@ -150,24 +150,63 @@
 // };
 
 // Redux - 9: Register a Store Listener
-const ADD = 'ADD';
-const reducer = (state = 0, action) => {
-  switch(action.type) {
-    case ADD:
-      return state + 1;
-    default:
-      return state;
-  }
-};
-const store = Redux.createStore(reducer);
-// global count variable:
-let count = 0;
-// change code below this line
-store.subscribe(() => { count++ });
-// change code above this line
-store.dispatch({type: ADD});
-console.log(count);
-store.dispatch({type: ADD});
-console.log(count);
-store.dispatch({type: ADD});
-console.log(count);
+// const ADD = 'ADD';
+// const reducer = (state = 0, action) => {
+//   switch(action.type) {
+//     case ADD:
+//       return state + 1;
+//     default:
+//       return state;
+//   }
+// };
+// const store = Redux.createStore(reducer);
+// // global count variable:
+// let count = 0;
+// // change code below this line
+// store.subscribe(() => { count++ });
+// // change code above this line
+// store.dispatch({type: ADD});
+// console.log(count);
+// store.dispatch({type: ADD});
+// console.log(count);
+// store.dispatch({type: ADD});
+// console.log(count);
+
+// Redux - 10: Combine Multiple Reducers
+// There are counterReducer() and authReducer() functions provided in the code editor,
+// along with a Redux store. Finish writing the rootReducer() function using
+// the Redux.combineReducers() method.
+// Assign counterReducer to a key called count and authReducer to a key called auth.
+// const INCREMENT = 'INCREMENT';
+// const DECREMENT = 'DECREMENT';
+// const counterReducer = (state = 0, action) => {
+//   switch(action.type) {
+//     case INCREMENT:
+//       return state + 1;
+//     case DECREMENT:
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
+// const LOGIN = 'LOGIN';
+// const LOGOUT = 'LOGOUT';
+// const authReducer = (state = {authenticated: false}, action) => {
+//   switch(action.type) {
+//     case LOGIN:
+//       return {
+//         authenticated: true
+//       }
+//     case LOGOUT:
+//       return {
+//         authenticated: false
+//       }
+//     default:
+//       return state;
+//   }
+// };
+// const rootReducer = Redux.combineReducers({
+//   count: counterReducer,
+//   auth: authReducer
+// });
+// const store = Redux.createStore(rootReducer);
