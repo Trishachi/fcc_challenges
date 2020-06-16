51a3d9ecfe2ca642393efa0639ce33835cc2e1c3
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-15 23:51:17 -0600

    Redux - Use the Spread Operator on Arrays

--- Redux.js
+++ Redux.js
@@ -309,27 +309,47 @@
 // const store = Redux.createStore(counterReducer);; // define the Redux store here, passing in your reducers
 
 // Redux - 14: Never Mutate State
-const ADD_TO_DO = 'ADD_TO_DO';
-// A list of strings representing tasks to do:
-const todos = [
-  'Go to the store',
-  'Clean the house',
-  'Cook dinner',
-  'Learn to code',
-];
-const immutableReducer = (state = todos, action) => {
+// const ADD_TO_DO = 'ADD_TO_DO';
+// // A list of strings representing tasks to do:
+// const todos = [
+//   'Go to the store',
+//   'Clean the house',
+//   'Cook dinner',
+//   'Learn to code',
+// ];
+// const immutableReducer = (state = todos, action) => {
+//   switch(action.type) {
+//     case ADD_TO_DO:
+//       // don't mutate state here or the tests will fail
+//       return todos.concat(action.todo)
+//     default:
+//       return state;
+//   }
+// };
+// // an example todo argument would be 'Learn React',
+// const addToDo = (todo) => {
+//   return {
+//     type: ADD_TO_DO,
+//     todo
+//   }
+// }
+// const store = Redux.createStore(immutableReducer);
+
+// Redux - 15: Use the Spread Operator on Arrays
+// Use the spread operator to return a new copy of state when a to-do is added.
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
   switch(action.type) {
-    case ADD_TO_DO:
+    case 'ADD_TO_DO':
       // don't mutate state here or the tests will fail
-      return todos.concat(action.todo)
+      let myArr = [...state, action.todo];
+      return myArr;
     default:
       return state;
   }
 };
-// an example todo argument would be 'Learn React',
 const addToDo = (todo) => {
   return {
-    type: ADD_TO_DO,
+    type: 'ADD_TO_DO',
     todo
   }
 }
