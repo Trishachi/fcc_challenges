88f5de65f2bf1feb7296ed720a04dceeeb55c389
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-15 18:06:55 -0600

    Redux - Use Middleware to Handle Asynchronous Actions

--- Redux.js
+++ Redux.js
@@ -212,26 +212,72 @@
 // const store = Redux.createStore(rootReducer);
 
 // Redux - 11: Send Action Data to the Store
-const ADD_NOTE = 'ADD_NOTE';
-const notesReducer = (state = 'Initial State', action) => {
+// const ADD_NOTE = 'ADD_NOTE';
+// const notesReducer = (state = 'Initial State', action) => {
+//   switch(action.type) {
+//     // change code below this line
+// case ADD_NOTE:
+//   return action.text
+//     // change code above this line
+//     default:
+//       return state;
+//   }
+// };
+// const addNoteText = (note) => {
+//   // change code below this line
+// return {
+//   type: ADD_NOTE,
+//   text: note
+// }
+//   // change code above this line
+// };
+// const store = Redux.createStore(notesReducer);
+// console.log(store.getState());
+// store.dispatch(addNoteText('Hello!'));
+// console.log(store.getState());
+
+// Redux - 12: Use Middleware to Handle Asynchronous Actions
+const REQUESTING_DATA = 'REQUESTING_DATA'
+const RECEIVED_DATA = 'RECEIVED_DATA'
+const requestingData = () => { return {type: REQUESTING_DATA} }
+const receivedData = (data) => { return {type: RECEIVED_DATA, users: data.users} }
+const handleAsync = () => {
+  return function(dispatch) {
+    // dispatch request action here
+    dispatch(requestingData());
+    setTimeout(function() {
+      let data = {
+        users: ['Jeff', 'William', 'Alice']
+      }
+      // dispatch received data action here
+    dispatch(receivedData(data));
+    }, 2500);
+  }
+};
+
+const defaultState = {
+  fetching: false,
+  users: []
+};
+
+const asyncDataReducer = (state = defaultState, action) => {
   switch(action.type) {
-    // change code below this line
-case ADD_NOTE:
-  return action.text
-    // change code above this line
+    case REQUESTING_DATA:
+      return {
+        fetching: true,
+        users: []
+      }
+    case RECEIVED_DATA:
+      return {
+        fetching: false,
+        users: action.users
+      }
     default:
       return state;
   }
 };
-const addNoteText = (note) => {
-  // change code below this line
-return {
-  type: ADD_NOTE,
-  text: note
-}
-  // change code above this line
-};
-const store = Redux.createStore(notesReducer);
-console.log(store.getState());
-store.dispatch(addNoteText('Hello!'));
-console.log(store.getState());
+
+const store = Redux.createStore(
+  asyncDataReducer,
+  Redux.applyMiddleware(ReduxThunk.default)
+);
