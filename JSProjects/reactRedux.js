76a7ae2dc6b84bbf5a97fa56dc6a149622fa598c
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-16 00:38:01 -0600

    Getting Started with React Redux

---
+++ JSProjects/reactRedux.js
@@ -0,0 +1,11 @@
+//Notes
+// React is a view library that you provide with data, then it renders the view in an efficient, predictable way.
+// Redux is a state management framework that you can use to simplify the management of your application's state.
+// Typically, in a React Redux app, you create a single Redux store that manages the state of your entire app.
+// Your React components subscribe to only the pieces of data in the store that are relevant to their role.
+// Then, you dispatch actions directly from React components, which then trigger store updates.
+
+
+
+//Codes
+React and Redux - 1: Getting Started with React Redux
