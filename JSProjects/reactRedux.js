584d0ad0412468df42d530c1ce53470f076e3cc5
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-18 01:39:26 -0600

    Managing State locally

--- JSProjects/reactRedux.js
+++ JSProjects/reactRedux.js
@@ -8,4 +8,59 @@
 
 
 //Codes
-React and Redux - 1: Getting Started with React Redux
+// React and Redux - 1: Getting Started with React Redux
+// class DisplayMessages extends React.Component {
+//   // change code below this line
+//   constructor(props){
+//     super(props);
+//     this.state = {
+//       input: "",
+//       messages: []
+//     }
+//   }
+//   // change code above this line
+//   render() {
+//     return <div />
+//   }
+// };
+
+React and Redux - 2: Manage State Locally First
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  // add handleChange() and submitMessage() methods here
+handleChange(event){
+  this.setState({input: event.target.value,
+  messages: this.state.messages });
+}
+submitMessage(){
+  this.setState({input: "" ,messages: [...this.state.messages, this.state.input]});
+}
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* render an input, button, and ul here */ }
+<input
+        type='text'
+        onChange={this.handleChange}
+        value={this.state.input}
+      />
+      <button  onClick={this.submitMessage}>Click Me!</button>
+      <ul>
+        {this.state.messages.map((x, i)=>{
+            return <li key={i}>{x}</li>
+          })}
+      </ul>
+        { /* change code above this line */ }
+</div>
+    );
+  }
+};
