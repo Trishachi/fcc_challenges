a66e003fa3330104035bcc02d42a1a695c10f468
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-18 13:54:58 -0600

    Extract State Logic to Redux

--- JSProjects/reactRedux.js
+++ JSProjects/reactRedux.js
@@ -24,43 +24,63 @@
 //   }
 // };
 
-React and Redux - 2: Manage State Locally First
-class DisplayMessages extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: '',
-      messages: []
-    }
-    this.handleChange = this.handleChange.bind(this);
-    this.submitMessage = this.submitMessage.bind(this);
-  }
-  // add handleChange() and submitMessage() methods here
-handleChange(event){
-  this.setState({input: event.target.value,
-  messages: this.state.messages });
-}
-submitMessage(){
-  this.setState({input: "" ,messages: [...this.state.messages, this.state.input]});
-}
-  render() {
-    return (
-      <div>
-        <h2>Type in a new Message:</h2>
-        { /* render an input, button, and ul here */ }
-<input
-        type='text'
-        onChange={this.handleChange}
-        value={this.state.input}
-      />
-      <button  onClick={this.submitMessage}>Click Me!</button>
-      <ul>
-        {this.state.messages.map((x, i)=>{
-            return <li key={i}>{x}</li>
-          })}
-      </ul>
-        { /* change code above this line */ }
-</div>
-    );
-  }
-};
+// React and Redux - 2: Manage State Locally First
+// class DisplayMessages extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       input: '',
+//       messages: []
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.submitMessage = this.submitMessage.bind(this);
+//   }
+//   // add handleChange() and submitMessage() methods here
+// handleChange(event){
+//   this.setState({input: event.target.value,
+//   messages: this.state.messages });
+// }
+// submitMessage(){
+//   this.setState({input: "" ,messages: [...this.state.messages, this.state.input]});
+// }
+//   render() {
+//     return (
+//       <div>
+//         <h2>Type in a new Message:</h2>
+//         { /* render an input, button, and ul here */ }
+// <input
+//         type='text'
+//         onChange={this.handleChange}
+//         value={this.state.input}
+//       />
+//       <button  onClick={this.submitMessage}>Click Me!</button>
+//       <ul>
+//         {this.state.messages.map((x, i)=>{
+//             return <li key={i}>{x}</li>
+//           })}
+//       </ul>
+//         { /* change code above this line */ }
+// </div>
+//     );
+//   }
+// };
+
+// React and Redux - 3: Extract State Logic to Redux
+// // define ADD, addMessage(), messageReducer(), and store here:
+// const ADD = 'ADD';
+// const addMessage = message =>{
+//   return {
+//     type: ADD,
+//     message
+//   };
+// };
+// const messageReducer = (initialState = [], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [...initialState, action.message];
+//       break;
+//     default:
+//       return initialState;
+//   }
+// };
+// const store = Redux.createStore(messageReducer);
