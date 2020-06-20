875f8039a16d1e3f46b684d52be6a06022fa7813
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-20 16:26:37 -0600

    React and Redux - 6: Map Dispatch to Props

--- JSProjects/reactRedux.js
+++ JSProjects/reactRedux.js
@@ -87,77 +87,101 @@
 
 // React and Redux - 4: Use Provider to Connect Redux to React
 // Redux Code:
-const ADD = 'ADD';
+// const ADD = 'ADD';
+// const addMessage = (message) => {
+//   return {
+//     type: ADD,
+//     message
+//   }
+// };
+// const messageReducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [
+//         ...state,
+//         action.message
+//       ];
+//     default:
+//       return state;
+//   }
+// };
+// const store = Redux.createStore(messageReducer);
+// // React Code:
+// class DisplayMessages extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       input: '',
+//       messages: []
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.submitMessage = this.submitMessage.bind(this);
+//   }
+//   handleChange(event) {
+//     this.setState({
+//       input: event.target.value
+//     });
+//   }
+//   submitMessage() {
+//     const currentMessage = this.state.input;
+//     this.setState({
+//       input: '',
+//       messages: this.state.messages.concat(currentMessage)
+//     });
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h2>Type in a new Message:</h2>
+//         <input
+//           value={this.state.input}
+//           onChange={this.handleChange}/><br/>
+//         <button onClick={this.submitMessage}>Submit</button>
+//         <ul>
+//           {this.state.messages.map( (message, idx) => {
+//               return (
+//                  <li key={idx}>{message}</li>
+//               )
+//             })
+//           }
+//         </ul>
+//       </div>
+//     );
+//   }
+// };
+// const Provider = ReactRedux.Provider;
+// class AppWrapper extends React.Component {
+//   // render the Provider here
+// render(){
+//   return(
+//     <Provider store={store}>
+//       <DisplayMessages />
+//     </Provider>
+//   );
+// }
+//   // change code above this line
+// };
+
+// React and Redux - 5: Map State to Props
+// const state = [];
+// // change code below this line
+// function mapStateToProps(state){
+// return {
+//   messages: state
+// }
+// };
+
+// React and Redux - 6: Map Dispatch to Props
 const addMessage = (message) => {
   return {
-    type: ADD,
-    message
+    type: 'ADD',
+    message: message
   }
 };
-const messageReducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD:
-      return [
-        ...state,
-        action.message
-      ];
-    default:
-      return state;
-  }
-};
-const store = Redux.createStore(messageReducer);
-// React Code:
-class DisplayMessages extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      input: '',
-      messages: []
+// change code below this line
+function mapDispatchToProps(dispatch){
+  return {submitNewMessage: (message)=> {
+    dispatch(addMessage(message))
     }
-    this.handleChange = this.handleChange.bind(this);
-    this.submitMessage = this.submitMessage.bind(this);
   }
-  handleChange(event) {
-    this.setState({
-      input: event.target.value
-    });
-  }
-  submitMessage() {
-    const currentMessage = this.state.input;
-    this.setState({
-      input: '',
-      messages: this.state.messages.concat(currentMessage)
-    });
-  }
-  render() {
-    return (
-      <div>
-        <h2>Type in a new Message:</h2>
-        <input
-          value={this.state.input}
-          onChange={this.handleChange}/><br/>
-        <button onClick={this.submitMessage}>Submit</button>
-        <ul>
-          {this.state.messages.map( (message, idx) => {
-              return (
-                 <li key={idx}>{message}</li>
-              )
-            })
-          }
-        </ul>
-      </div>
-    );
-  }
-};
-const Provider = ReactRedux.Provider;
-class AppWrapper extends React.Component {
-  // render the Provider here
-render(){
-  return(
-    <Provider store={store}>
-      <DisplayMessages />
-    </Provider>
-  );
 }
-  // change code above this line
-};
