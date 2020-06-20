f55211d88a1994218af2cbbd23e08a32450c334f
Chisom Trisha Okoye <trisha.omabu@gmail.com>
2020-06-20 16:36:08 -0600

    React and Redux: Connect Redux to React

--- JSProjects/reactRedux.js
+++ JSProjects/reactRedux.js
@@ -172,16 +172,47 @@
 // };
 
 // React and Redux - 6: Map Dispatch to Props
+// const addMessage = (message) => {
+//   return {
+//     type: 'ADD',
+//     message: message
+//   }
+// };
+// // change code below this line
+// function mapDispatchToProps(dispatch){
+//   return {submitNewMessage: (message)=> {
+//     dispatch(addMessage(message))
+//     }
+//   }
+// }
+
+// React and Redux - 7: Connect Redux to React
 const addMessage = (message) => {
   return {
     type: 'ADD',
     message: message
   }
 };
-// change code below this line
-function mapDispatchToProps(dispatch){
-  return {submitNewMessage: (message)=> {
-    dispatch(addMessage(message))
+const mapStateToProps = (state) => {
+  return {
+    messages: state
+  }
+};
+const mapDispatchToProps = (dispatch) => {
+  return {
+    submitNewMessage: (message) => {
+      dispatch(addMessage(message));
     }
   }
-}
+};
+class Presentational extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <h3>This is a Presentational Component</h3>
+  }
+};
+const connect = ReactRedux.connect;
+// change code below this line
+const ConnectedComponent = connect(mapStateToProps, mapDispatchToProps)(Presentational);
